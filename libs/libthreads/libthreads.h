171599f2af9f67fd68478bec45b360988a398bf2
Louis Wadbled <louis.wadbled@polytech-lille.net>
2021-03-04 11:38:55 +0100

    fix(libthreads.h): correction d une erreur

--- libs/libthreads/libthreads.h
+++ libs/libthreads/libthreads.h
@@ -16,7 +16,6 @@ typedef struct {
 } _internal_type;
 
 /* PROTOTYPES */
-static void *_launchThread(void *arg);
 void launchThread(void (*function)(void *),void *argument,int size);
 
 #endif //LIBTHREADS_H
